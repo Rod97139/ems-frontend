eb0cd57fc37be6e6d4ede1f9dece341f1c9f92ae
Rod97139 <fayaflame@gmail.com>
2024-04-21 20:15:53 +0200

    update employee title and redirection

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
     const navigator = useNavigate()
+
+    const {id} = useParams()
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
@@ -56,12 +58,20 @@ const EmployeeComponent = () => {
         return valid
     }
 
+    const pageTitle = () => {
+        if (id) {
+            return <h2 className='text-center'>Update Employee</h2>
+        } else {
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container' >
         <br /><br />
         <div className='row'>
             <div className="card col-md-6 offset-md-3">
-                <h2 className='text-center' >Add Employee</h2>
+                {pageTitle()}
                 <div className="card-body">
                     <form>
                         <div className="form-group mb-2">
--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -19,6 +19,10 @@ const ListEmployeeComponent = () => {
         navigator('/add-employee');
     }
 
+    const updateEmployee = (id) => {
+        navigator(`/edit-employee/${id}`);
+    }
+
   return (
     <div className='container'>
       <h2 className='text-center'>List of Employees</h2>
@@ -30,6 +34,7 @@ const ListEmployeeComponent = () => {
                 <th>Employee First Name</th>
                 <th>Employee Last Name</th>
                 <th>Employee Email Id</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -41,6 +46,10 @@ const ListEmployeeComponent = () => {
                         <td>{employee.firstName}</td>
                         <td>{employee.lastName}</td>
                         <td>{employee.email}</td>
+                        <td>
+                            <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                            <button className='btn btn-danger ms-1'>Delete</button>
+                        </td>
                     </tr>
                 )
             }
