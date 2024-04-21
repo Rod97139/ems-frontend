4337278aa73b62d9318f25b803cc1cb85c538243
Rod97139 <fayaflame@gmail.com>
2024-04-21 17:20:17 +0200

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   return (
     <>
       <h1 className='text-center' >Ems front-end</h1>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer>
+        <span className='footer'>All Rights Reserved 2024 by RM</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
